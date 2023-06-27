63fc12f85c9ad81aad05f2c3e25f3ac2b395f876
Purushuttam Kumar <55408162+purushuttam@users.noreply.github.com>
2023-06-27 22:13:54 +0530

    Update connectDB.go

--- initializers/connectDB.go
+++ initializers/connectDB.go
@@ -14,7 +14,7 @@ func ConnectDB(config *Config) {
 	var err error
 	//dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=require TimeZone=Asia/Shanghai", config.DBHost, config.DBUserName, config.DBUserPassword, config.DBName, config.DBPort)
 
-	DB, err = gorm.Open(postgres.Open("postgres://postgres_9q1x_user:YQP68gEY4vlDRZAfzBsT7culK5V9815V@dpg-cica2d95rnuk9qbcmicg-a/postgres_9q1x"), &gorm.Config{})
+	DB, err = gorm.Open(postgres.Open("postgres://purushuttam:cH8iCTR5nfpgVd9oOE9wlMMYCvcj9EgX@dpg-cidh30enqqlb62jn9jdg-a.singapore-postgres.render.com/restrauntdb"), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Failed to connect to the Database")
 	}
